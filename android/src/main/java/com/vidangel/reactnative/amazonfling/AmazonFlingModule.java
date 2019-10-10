f3e2948567ffa1ef3c3c8428cf08976f823a6b2c
jedashford <jedashford@gmail.com>
2019-10-10 12:32:25 -0600

    add manifest and initial test of device discovery

---
+++ android/src/main/java/com/vidangel/reactnative/amazonfling/AmazonFlingModule.java
@@ -0,0 +1,51 @@
+package com.vidangel.reactnative.amazonfling;
+
+import android.util.Log;
+
+import com.amazon.whisperplay.fling.media.controller.DiscoveryController;
+import com.amazon.whisperplay.fling.media.controller.RemoteMediaPlayer;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+public class AmazonFlingModule extends ReactContextBaseJavaModule {
+
+    private DiscoveryController mController;
+    private final ReactApplicationContext reactContext;
+
+    public AmazonFlingModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        mController = new DiscoveryController(reactContext);
+
+    }
+
+    @Override
+    public String getName() {
+        return "AmazonFling";
+    }
+
+    @ReactMethod
+    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
+        // TODO: Implement some actually useful functionality
+        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+
+    private DiscoveryController.IDiscoveryListener mDiscovery = new DiscoveryController.IDiscoveryListener() {
+        @Override
+        public void playerDiscovered(RemoteMediaPlayer player) {
+            Log.e("jed", "playerDiscovered" + player.toString());
+            //add media player to the application’s player list.
+        }
+        @Override
+        public void playerLost(RemoteMediaPlayer player) {
+            Log.e("jed", "playerLost" + player.toString());
+            //remove media player from the application’s player list.
+        }
+        @Override
+        public void discoveryFailure() {
+            Log.e("jed", "discoveryFailure");
+        }
+    };
+}
--- android/src/main/java/com/vidangel/reactnative/amazonfling/AmazonFlingPackage.java
+++ android/src/main/java/com/vidangel/reactnative/amazonfling/AmazonFlingPackage.java
@@ -1,6 +1,5 @@
-package com.reactlibrary;
+package com.vidangel.reactnative.amazonfling;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -9,7 +8,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
 public class AmazonFlingPackage implements ReactPackage {
     @Override
