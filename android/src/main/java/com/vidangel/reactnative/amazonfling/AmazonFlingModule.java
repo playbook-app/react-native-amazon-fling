d5dd88e2bd8af6e71d0770f96641aa171b2923a1
jedashford <jedashford@gmail.com>
2019-10-10 16:34:43 -0600

    attempt discovery test

--- android/src/main/java/com/vidangel/reactnative/amazonfling/AmazonFlingModule.java
+++ android/src/main/java/com/vidangel/reactnative/amazonfling/AmazonFlingModule.java
@@ -4,44 +4,50 @@ import android.util.Log;
 
 import com.amazon.whisperplay.fling.media.controller.DiscoveryController;
 import com.amazon.whisperplay.fling.media.controller.RemoteMediaPlayer;
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
-public class AmazonFlingModule extends ReactContextBaseJavaModule {
+public class AmazonFlingModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
     private final ReactApplicationContext reactContext;
 
     public AmazonFlingModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        reactContext.addLifecycleEventListener(this);
+        mController = new DiscoveryController(reactContext.getApplicationContext());
 //        mController = new DiscoveryController(reactContext);
 //        mController.start("amzn.thin.pl", mDiscovery);
 
     }
+
+    private DiscoveryController mController;
+
+    private DiscoveryController.IDiscoveryListener mDiscovery = new DiscoveryController.IDiscoveryListener() {
+        @Override
+        public void playerDiscovered(RemoteMediaPlayer player) {
+            Log.e("jedtest", "jed1" + player.toString());
+            //add media player to the application’s player list.
+        }
+        @Override
+        public void playerLost(RemoteMediaPlayer player) {
+            Log.e("jedtest", "jed2");
+            //remove media player from the application’s player list.
+        }
+        @Override
+        public void discoveryFailure() {
+            Log.e("jedtest", "jed3");
+        }
+    };
+
     @ReactMethod
     public void search() {
+
         Log.e("jedsearch", "jedsearch");
-        DiscoveryController mController;
-        DiscoveryController.IDiscoveryListener mDiscovery = new DiscoveryController.IDiscoveryListener() {
-            @Override
-            public void playerDiscovered(RemoteMediaPlayer player) {
-                Log.e("jedsearch", "playerDiscovered");
-                Log.e("jed", "playerDiscovered" + player.toString());
-                //add media player to the application’s player list.
-            }
-            @Override
-            public void playerLost(RemoteMediaPlayer player) {
-                Log.e("jedsearch", "playerLost");
-                Log.e("jed", "playerLost" + player.toString());
-                //remove media player from the application’s player list.
-            }
-            @Override
-            public void discoveryFailure() {
-                Log.e("jed", "discoveryFailure");
-            }
-        };
+
         Log.e("jedsearch", "jedsearch1");
         mController = new DiscoveryController(reactContext.getApplicationContext());
         Log.e("jedsearch", "jedsearch2");
@@ -60,4 +66,18 @@ public class AmazonFlingModule extends ReactContextBaseJavaModule {
     }
 
 
+    @Override
+    public void onHostResume() {
+        mController.start("amzn.thin.pl", mDiscovery);
+    }
+
+    @Override
+    public void onHostPause() {
+        mController.stop();
+    }
+
+    @Override
+    public void onHostDestroy() {
+
+    }
 }
