eef237ae1d405e844f2c85bcef14ba21f88b28a7
jedashford <jedashford@gmail.com>
2019-10-11 13:11:25 -0600

    testing changes

--- android/src/main/java/com/vidangel/reactnative/amazonfling/AmazonFlingModule.java
+++ android/src/main/java/com/vidangel/reactnative/amazonfling/AmazonFlingModule.java
@@ -1,7 +1,6 @@
 package com.vidangel.reactnative.amazonfling;
 
 import android.util.Log;
-import android.widget.Toast;
 
 import com.amazon.whisperplay.fling.media.controller.DiscoveryController;
 import com.amazon.whisperplay.fling.media.controller.RemoteMediaPlayer;
@@ -10,22 +9,48 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+import com.google.gson.Gson;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-
-import static com.facebook.react.bridge.UiThreadUtil.runOnUiThread;
 
 public class AmazonFlingModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
     private final ReactApplicationContext reactContext;
     private static final long MONITOR_INTERVAL = 1000L;
+    private List<RemoteMediaPlayer> mDeviceList = new LinkedList<>();
 
     private DiscoveryController mController;
 
+    private DiscoveryController.IDiscoveryListener mDiscovery = new DiscoveryController.IDiscoveryListener() {
+        @Override
+        public void playerDiscovered(RemoteMediaPlayer player) {
+            Log.e("jedtest", "playerDiscovered" + player.toString());
+//                fling(player, "https://video.thechosen.tv/The_Chosen_S01E01_patch50219.m3u8", "Episode 1");
+            //add media player to the application’s player list.
+            updateDeviceList(player);
+        }
+
+        @Override
+        public void playerLost(RemoteMediaPlayer player) {
+            Log.e("jedtest", "jed2");
+            //remove media player from the application’s player list.
+        }
+
+        @Override
+        public void discoveryFailure() {
+            Log.e("jedtest", "jed3");
+        }
+    };
+
     public AmazonFlingModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        Log.e("jedsearch", "AmazonFlingModule");
+        Log.e("jedsearch", "AmazonFlingModule5599");
         this.reactContext = reactContext;
         reactContext.addLifecycleEventListener(this);
 //        mController = new DiscoveryController(reactContext);
@@ -34,14 +59,22 @@ public class AmazonFlingModule extends ReactContextBaseJavaModule implements Lif
     }
 
     @ReactMethod
-    public void search() {
+    public void startSearch() {
+        Log.e("jedsearch55", "jedsearch00");
+        mController = new DiscoveryController(reactContext.getBaseContext());
+        mController.start("amzn.thin.pl", mDiscovery);
+        Log.e("jedsearch55", "jedsearch");
 
-        Log.e("jedsearch", "jedsearch");
-
-        Log.e("jedsearch", "jedsearch1");
+        Log.e("jedsearch55", "jedsearch1");
 //        mController = new DiscoveryController(reactContext.getApplicationContext());
-        Log.e("jedsearch", "jedsearch2");
+        Log.e("jedsearch55", "jedsearch2");
 //        mController.start("amzn.thin.pl", mDiscovery);
+    }
+
+    @ReactMethod
+    public void stopSearch() {
+        Log.e("jedsearch", "stop");
+        mController.stop();
     }
 
     @Override
@@ -51,35 +84,31 @@ public class AmazonFlingModule extends ReactContextBaseJavaModule implements Lif
 
     @ReactMethod
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
+        Log.e("jedsearch", "sampleMethod");
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
     }
 
-
-    @Override
-    public void onHostResume() {
-        Log.e("jedsearch", "onHostResume");
-        mController = new DiscoveryController(reactContext.getBaseContext());
-        DiscoveryController.IDiscoveryListener mDiscovery = new DiscoveryController.IDiscoveryListener() {
-            @Override
-            public void playerDiscovered(RemoteMediaPlayer player) {
-                Log.e("jedtest", "jed1" + player.toString());
-                fling(player, "https://video.thechosen.tv/The_Chosen_S01E01_patch50219.m3u8", "Episode 1");
-                //add media player to the application’s player list.
-            }
-            @Override
-            public void playerLost(RemoteMediaPlayer player) {
-                Log.e("jedtest", "jed2");
-                //remove media player from the application’s player list.
-            }
-            @Override
-            public void discoveryFailure() {
-                Log.e("jedtest", "jed3");
-            }
-        };
-        mController.start("amzn.thin.pl", mDiscovery);
+    @ReactMethod
+    public void test() {
+        Log.e("jedsearch", "test");
+        // TODO: Implement some actually useful functionality
     }
 
+    public void updateDeviceList(RemoteMediaPlayer device){
+        Log.e("jedsearch55", "updateDeviceList");
+        mDeviceList.add(device);
+        String jsonInString = new Gson().toJson(mDeviceList);
+        WritableMap params = Arguments.createMap();
+        params.putString("devices", jsonInString);
+        sendEvent("device_list", params);
+    }
+
+    private void sendEvent(String eventName, WritableMap params) {
+        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
+    }
+
+    @ReactMethod
     private void fling(final RemoteMediaPlayer target, final String name, final String title) {
 //        initializeFling(target);
         Log.i("jed", "try setPositionUpdateInterval: " + MONITOR_INTERVAL);
@@ -89,17 +118,18 @@ public class AmazonFlingModule extends ReactContextBaseJavaModule implements Lif
         Log.i("jed", "try setMediaSource: url - " + name + " title - " + title);
         target.setMediaSource(name, title, true, false).getAsync(
                 new ErrorResultHandler("setMediaSource", "Error attempting to Play:", true));
-        showToast("try Flinging...");
+//        showToast("try Flinging...");
     }
 
-    private void showToast(final String msg) {
-        runOnUiThread(new Runnable() {
-            public void run() {
-                Toast toast = Toast.makeText(reactContext.getApplicationContext(), msg, Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
-    }
+//    private void showToast(final String msg) {
+//        runOnUiThread(new Runnable() {
+//            public void run() {
+//                Toast toast = Toast.makeText(reactContext.getApplicationContext(), msg, Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
+//    }
+
     private class ErrorResultHandler implements RemoteMediaPlayer.FutureListener<Void> {
         private String mCommand;
         private String mMsg;
@@ -119,25 +149,30 @@ public class AmazonFlingModule extends ReactContextBaseJavaModule implements Lif
         public void futureIsNow(Future<Void> result) {
             try {
                 result.get();
-                showToast(mCommand);
+//                showToast(mCommand);
 //                mErrorCount = 0;
                 Log.i("jed", mCommand + ": successful");
-            } catch(ExecutionException e) {
+            } catch (ExecutionException e) {
 //                handleFailure(e.getCause(), mMsg, mExtend);
-            } catch(Exception e) {
+            } catch (Exception e) {
 //                handleFailure(e, mMsg, mExtend);
             }
         }
     }
 
     @Override
-    public void onHostPause() {
-        Log.e("jedsearch", "onHostPause");
-        mController.stop();
+    public void onHostDestroy() {
+        stopSearch();
     }
 
     @Override
-    public void onHostDestroy() {
+    public void onHostResume() {
+        //do nothing
 
+    }
+
+    @Override
+    public void onHostPause() {
+        //do nothing
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import { NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 
 const { AmazonFling } = NativeModules;
+
+const eventEmitter = new NativeEventEmitter(AmazonFling);
+
+const listenEvents = () => {
+  eventEmitter.addListener('device_list', (event) => {
+    console.log('event: device_list', event)
+  })
+}
+
 
 export default AmazonFling;
