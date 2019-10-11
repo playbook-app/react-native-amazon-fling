19543a55a7a75df91a41b597ef038f7f051fbb96
jedashford <jedashford@gmail.com>
2019-10-10 21:29:32 -0600

    add more api

--- android/src/main/java/com/vidangel/reactnative/amazonfling/AmazonFlingModule.java
+++ android/src/main/java/com/vidangel/reactnative/amazonfling/AmazonFlingModule.java
@@ -1,6 +1,7 @@
 package com.vidangel.reactnative.amazonfling;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.amazon.whisperplay.fling.media.controller.DiscoveryController;
 import com.amazon.whisperplay.fling.media.controller.RemoteMediaPlayer;
@@ -10,9 +11,15 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static com.facebook.react.bridge.UiThreadUtil.runOnUiThread;
+
 public class AmazonFlingModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
 
     private final ReactApplicationContext reactContext;
+    private static final long MONITOR_INTERVAL = 1000L;
 
     private DiscoveryController mController;
 
@@ -57,6 +64,7 @@ public class AmazonFlingModule extends ReactContextBaseJavaModule implements Lif
             @Override
             public void playerDiscovered(RemoteMediaPlayer player) {
                 Log.e("jedtest", "jed1" + player.toString());
+                fling(player, "https://video.thechosen.tv/The_Chosen_S01E01_patch50219.m3u8", "Episode 1");
                 //add media player to the application’s player list.
             }
             @Override
@@ -70,6 +78,56 @@ public class AmazonFlingModule extends ReactContextBaseJavaModule implements Lif
             }
         };
         mController.start("amzn.thin.pl", mDiscovery);
+    }
+
+    private void fling(final RemoteMediaPlayer target, final String name, final String title) {
+//        initializeFling(target);
+        Log.i("jed", "try setPositionUpdateInterval: " + MONITOR_INTERVAL);
+        target.setPositionUpdateInterval(MONITOR_INTERVAL).getAsync(
+                new ErrorResultHandler("setPositionUpdateInterval",
+                        "Error attempting set update interval, ignoring", true));
+        Log.i("jed", "try setMediaSource: url - " + name + " title - " + title);
+        target.setMediaSource(name, title, true, false).getAsync(
+                new ErrorResultHandler("setMediaSource", "Error attempting to Play:", true));
+        showToast("try Flinging...");
+    }
+
+    private void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast toast = Toast.makeText(reactContext.getApplicationContext(), msg, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+    private class ErrorResultHandler implements RemoteMediaPlayer.FutureListener<Void> {
+        private String mCommand;
+        private String mMsg;
+        private boolean mExtend;
+
+        ErrorResultHandler(String command, String msg) {
+            this(command, msg, false);
+        }
+
+        ErrorResultHandler(String command, String msg, boolean extend) {
+            mCommand = command;
+            mMsg = msg;
+            mExtend = extend;
+        }
+
+        @Override
+        public void futureIsNow(Future<Void> result) {
+            try {
+                result.get();
+                showToast(mCommand);
+//                mErrorCount = 0;
+                Log.i("jed", mCommand + ": successful");
+            } catch(ExecutionException e) {
+//                handleFailure(e.getCause(), mMsg, mExtend);
+            } catch(Exception e) {
+//                handleFailure(e, mMsg, mExtend);
+            }
+        }
     }
 
     @Override
