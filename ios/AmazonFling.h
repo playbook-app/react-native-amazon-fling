46c5e81ff9ba266fa6e865554072a1f74cd36fa6
jedashford <jedashford@gmail.com>
2019-10-15 21:34:54 -0600

    Amazon fling for ios first test

--- ios/AmazonFling.h
+++ ios/AmazonFling.h
@@ -1,5 +1,11 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTLog.h>
+#import <AmazonFling/DiscoveryController.h>
+#import <AmazonFling/MediaPlayerStatus.h>
+#import <AmazonFling/RemoteMediaPlayer.h>
+#import <Bolts/BFTask.h>
 
-@interface AmazonFling : NSObject <RCTBridgeModule>
+@interface AmazonFling : RCTEventEmitter <RCTBridgeModule>
 
 @end
--- ios/AmazonFling.m
+++ ios/AmazonFling.m
@@ -1,14 +1,45 @@
 #import "AmazonFling.h"
 
-
 @implementation AmazonFling
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+
+- (NSArray<NSString *> *)supportedEvents
 {
-    // TODO: Implement some actually useful functionality
-	callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+  return @[@"device_list"];
+}
+
+
+RCT_EXPORT_METHOD(startSearch)
+{
+    RCTLogWarn(@"rctwarn startSearch");
+    [self sendEventWithName:@"device_list" body:@{@"devices": @"[{\"hello\":\"one\"}]"}];
+}
+
+RCT_EXPORT_METHOD(stopSearch)
+{
+    RCTLogWarn(@"rctwarn stopSearch");
+}
+
+//final String targetUuid, final String name, final String title
+RCT_EXPORT_METHOD(fling:(NSString *)targetUuid name:(NSString *)name title:(NSString *)title)
+{
+    RCTLogWarn(@"rctwarn fling");
+}
+
+RCT_EXPORT_METHOD(doPlay:(NSString *)targetUuid )
+{
+    RCTLogWarn(@"rctwarn doPlay");
+}
+
+RCT_EXPORT_METHOD(doPause:(NSString *)targetUuid )
+{
+    RCTLogWarn(@"rctwarn doPause");
+}
+RCT_EXPORT_METHOD(doStop:(NSString *)targetUuid )
+{
+    RCTLogWarn(@"rctwarn doStop");
 }
 
 @end
